dc9c229dd4a5e0444846d9478426b3ab7cf1e347
Adedokun Femi <adedokunfemi14@gmail.com>
2023-03-16 11:19:45 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  Entrypoint for this program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
