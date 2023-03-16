05305d81b1cbc49742dd1d13e89b51215bda81f3
Adedokun Femi <adedokunfemi14@gmail.com>
2023-03-16 10:57:43 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- *  Return: Always 0 (Success)
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline."
+ * return 0.
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return(0);
+}
