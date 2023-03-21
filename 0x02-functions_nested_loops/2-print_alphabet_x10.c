a06cd6988f62ffb6b388728eff2886e8ab425dd6
Adedokun Femi <adedokunfemi14@gmail.com>
2023-03-21 05:29:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing a-z followed by a new line ten times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (letter = 'a'; letter <= 'z'; ++letter)
+		{
+			_putchar(letter);
+		}
+
+		_putchar('\n');
+	}
+}
