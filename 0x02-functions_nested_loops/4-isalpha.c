069e204efd620b5133779bd29aac552cc819483c
Adedokun Femi <Adedokunfemi14@gmail.com>
2023-03-21 13:52:15 +0100

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,8 @@
  * Return: 1 if character is a letter
  */
 int _isalpha(int c)
-}
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+	if ((c >= 'a' && c <= 'z) || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
