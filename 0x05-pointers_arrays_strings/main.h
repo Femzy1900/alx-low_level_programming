aac1b00d18e68cbd92b7ddf77bb2c6a6f6605036
Adedokun Femi <adedokunfemi14@gmail.com>
2023-03-28 01:12:49 -0700

    New change

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char.c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -16,4 +16,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H *
