2c8bb0b83d94e129bbd66772cced5284aaaf8fb7
Adedokun Femi <adedokunfemi14@gmail.com>
2023-04-05 14:54:39 -0700

    Qusetion 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: This is the string to be reversed
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+	}
+	else 
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
